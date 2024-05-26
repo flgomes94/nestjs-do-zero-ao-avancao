20b8235dbe84e81a0354d016d6f1408d7a77d018
Fabio Lopes <fabiolopes.dev@gmail.com>
2024-05-25 23:34:02 -0300

    criando post para usuário

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,9 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 
-@Controller()
+type ParamsUser = {
+  id: string;
+  idEmpresa: string;
+};
+
+type QueriesUser = {
+  page: string;
+};
+
+type BodyUser = {
+  name: string;
+  age: string;
+};
+
+@Controller('users')
 export class UserController {
   @Get('/helloUser')
   helloUser() {
     return 'Bem vindo ao curso de Nestjs';
+  }
+
+  @Get('/:id/:idEmpresa')
+  findById(@Param() params: ParamsUser) {
+    return 'Usuário de id ' + params.id + ' Empresa Id ' + params.idEmpresa;
+  }
+
+  @Get('/findByPage')
+  findByPages(@Query() queries: QueriesUser) {
+    return 'Queries ' + queries.page;
+  }
+
+  @Post('/create')
+  create(@Body() data: BodyUser) {
+    return { ...data, id: randomUUID() };
   }
 }
